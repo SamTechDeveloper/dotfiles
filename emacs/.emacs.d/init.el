ecab14fa09c336ee675abc04bb5f4a9f1de76e0f
SamTechDeveloper <samtechdeveloper@gmail.com>
2026-08-13 11:35:42 +0530

    emacs nongnu link error solved

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -19,7 +19,7 @@
 (require 'package)
 (setq package-archives
       '(("gnu"    . "https://elpa.gnu.org/packages/")
-        ("nongnu" . "https://elpa.nongnu.org/packages/")
+        ("nongnu" . "https://elpa.nongnu.org/nongnu/")
         ("melpa"  . "https://melpa.org/packages/")))
 (package-initialize)
 
